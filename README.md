2ca7b9db0d876669986948f1b8b46d5bcb68a111
Nitin Kumar <2018kumarnitin@gmail.com>
2023-09-18 13:38:06 +0530

    update read me

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# mini-wallet
+## ⚡ Quick Start
+  # Clone the project:
+  git clone git@github.com:NK8916/mini-wallet.git
+  cd mini-wallet
+
+  # Install the dependencies:
+  npm i
+
+  # Running the application:
+  npm run start:dev
+
+## Tech Stack Used
+  # Nodejs With Nestjs Framework
+  # Language:- Typescript
+  # Database:- Postgresql
+
+## Envrironment Variables
+  # DB_HOST
+  # DB_PORT
+  # DB_USER
+  # DB_PASSWORD
+  # DB_DATABASE
+  # JWT_SECRET
+
--- src/app.module.ts
+++ src/app.module.ts
@@ -11,11 +11,11 @@ import { AuthModule } from './auth/auth.module';
     ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: process.env.POSTGRES_HOST,
-      port: parseInt(<string>process.env.POSTGRES_PORT),
-      username: process.env.POSTGRES_USER,
-      password: process.env.POSTGRES_PASSWORD,
-      database: process.env.POSTGRES_DATABASE,
+      host: process.env.DB_HOST,
+      port: parseInt(<string>process.env.DB_PORT),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
       autoLoadEntities: true,
       ssl: true,
       synchronize: true, // shouldn't be used in production - may lose data
